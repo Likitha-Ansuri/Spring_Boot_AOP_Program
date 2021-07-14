9d54055cc470c7e157968ce0e7c6c262102b3810
Likitha-Ansuri <likitha.ansuri@ojas-it.com>
2021-07-14 15:28:55 +0530

    AOP Program

---
+++ src/main/java/com/example/SpringBootAop752022Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringBootAop752022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAop752022Application.class, args);
+	}
+
+}
